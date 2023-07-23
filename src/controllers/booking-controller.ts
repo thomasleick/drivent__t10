57d3d24442cb3a64469f4c0513740bfa7f7cd4da
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-07-22 21:52:50 -0300

    feat: .get /booking

---
+++ src/controllers/booking-controller.ts
@@ -0,0 +1,22 @@
+import { AuthenticatedRequest } from '@/middlewares';
+import { Response } from 'express';
+import httpStatus from 'http-status';
+import bookingService from '@/services/booking-service';
+
+export const getBookings = async (req: AuthenticatedRequest, res: Response) => {
+    const { userId } = req;
+
+    try {
+
+        const bookings = await bookingService.getBookings(userId)
+
+        if (!bookings) {
+            return res.sendStatus(httpStatus.NOT_FOUND)
+        }
+        return res.status(httpStatus.OK).send(bookings)
+
+    } catch (error) {
+        return res.sendStatus(httpStatus.BAD_REQUEST);
+    }
+
+}
