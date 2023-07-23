b6b761ecd9ec95e0b058b3b697b2e5c8cf51db46
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-07-23 14:45:18 -0300

    fix: .get /booking return correct format

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -7,13 +7,12 @@ export const getBookings = async (req: AuthenticatedRequest, res: Response) => {
     const { userId } = req;
 
     try {
-
-        const bookings = await bookingService.getBookings(userId)
+        const bookings = await bookingService.getBookings(userId);
 
         if (!bookings) {
-            return res.sendStatus(httpStatus.NOT_FOUND)
+            return res.sendStatus(httpStatus.NOT_FOUND);
         }
-        return res.status(httpStatus.OK).send(bookings)
+        return res.status(httpStatus.OK).send(bookings);
 
     } catch (error) {
         return res.sendStatus(httpStatus.BAD_REQUEST);
